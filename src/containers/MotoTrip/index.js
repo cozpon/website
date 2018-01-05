1661565bb1eb1d7dc926f5aac0899f4d0155897d
Ricky Peters <ericrickyallenpeters@gmail.com>
2018-01-04 14:45:02 -1000

    mototrip now has grid

--- src/containers/MotoTrip/index.js
+++ src/containers/MotoTrip/index.js
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/header.component';
-import Slider from 'react-viewport-slider';
+import StackGrid from "react-stack-grid";
+import sizeMe from 'react-sizeme';
 
 class MotoTrip extends Component {
   render() {
+    const { width } = this.props;
+
     return (
       <div>
       <Header />
-      <Slider>
-        <Slider.Item style={{ backgroundColor: '#666' }}>
+      <StackGrid
+        columnWidth={width <= 768 ? '100%' : '50%'}
+        monitorImagesLoaded={false} >
+        <div>
         <img src="http://payload309.cargocollective.com/1/12/398889/8499741/online01_1000.jpg"/>
-        </Slider.Item>
-
-        <Slider.Item style={{ backgroundColor: '#666' }}>
-        <img src="http://payload309.cargocollective.com/1/12/398889/8499741/Ricky_3_1000.jpeg"/>
-        </Slider.Item>
-
-        <Slider.Item style={{ backgroundColor: '#666' }}>
+        </div>
+        <div>
         <img src="http://payload309.cargocollective.com/1/12/398889/8499741/Ricky_10_1000.jpg"/>
-        </Slider.Item>
+        </div>
+        <div>
+        <img src="http://payload309.cargocollective.com/1/12/398889/8499741/Ricky_3_1000.jpeg"/>
+        </div>
+        <div>
+        <img src="http://payload309.cargocollective.com/1/12/398889/8499741/Untitled-2s_1000.jpg"/>
+        </div>
+        <div>
+        <img src="http://payload309.cargocollective.com/1/12/398889/8499741/Ricky_10_1000.jpg"/>
+        </div>
 
-        <Slider.Item style={{ backgroundColor: '#666' }}>
-        <img src="http://payload309.cargocollective.com/1/12/398889/8499741/Untitled-2w_1_1000.jpg"/>
-        </Slider.Item>
 
-        <Slider.Item style={{ backgroundColor: '#666' }}>
-        <img src="http://payload309.cargocollective.com/1/12/398889/8499741/oceanlake_1000.jpg"/>
-        </Slider.Item>
 
-        <Slider.Item style={{ backgroundColor: '#666' }}>
-        Motorcycle trip across the united states of america done alone with a hammock and a honda shadow
-        </Slider.Item>
-      </Slider>
-
-          <div id="art-button">
+        </StackGrid>
+        <div id="art-button">
             <Link to='/' style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
           </div>
       </div>
@@ -42,5 +41,5 @@ class MotoTrip extends Component {
   }
 }
 
-export default MotoTrip;
+export default sizeMe()(MotoTrip);
 
