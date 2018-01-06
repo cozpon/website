6515d3a487a0cd24d0a57bb443c3ba265fae447b
Ricky Peters <ericrickyallenpeters@gmail.com>
2018-01-05 18:55:55 -1000

    more images added

--- src/containers/MotoTrip/index.js
+++ src/containers/MotoTrip/index.js
@@ -14,46 +14,86 @@ class MotoTrip extends Component {
           columnWidth={width <= 800 ? '100%' : '50%'}
           monitorImagesLoaded={true} >
           <div>
-            <img src="/images/1.jpg"/>
+            <img src="/images/26.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/2.jpg"/>
+            <img src="/images/1.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/3.jpg"/>
+            <img src="/images/25.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/4.jpg"/>
+            <img src="/images/2.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/5.jpg"/>
+            <img src="/images/3.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/6.jpg"/>
+            <img src="/images/4.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/7.jpg"/>
+            <img src="/images/5.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/8.jpg"/>
+            <img src="/images/6.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/9.jpg"/>
+            <img src="/images/7.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/10.jpg"/>
+            <img src="/images/text2.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/11.jpg"/>
+            <img src="/images/9.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/12.jpg"/>
+            <img src="/images/10.jpg" alt=""/>
           </div>
           <div>
-            <img src="/images/text.jpg"/>
+            <img src="/images/11.jpg" alt=""/>
           </div>
-        </StackGrid>
-        <div id="art-button">
+          <div>
+            <img src="/images/12.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/8.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/13.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/14.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/15.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/16.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/17.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/18.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/19.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/20.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/22.jpg" alt=""/>
+          </div>
+          <div>
+            <img src="/images/23.jpg" alt=""/>
+          </div>
+          </StackGrid>
+          <div>
+            <img src="/images/24.jpg" alt=""/>
+          </div>
+
+        <div>
           <Link to='/' style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
         </div>
       </div>
